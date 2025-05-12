fd257b84935cecde87cbdfb09115c8c27c74ba5a
nabina-rumsan <nabina@rumsan.net>
2025-05-12 15:24:17 +0545

    browser window property

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Basic setup for an Electron app.
 ## Steps
 
 - Created local and remote GitHub repo  
-- Ran `npm init` and filled config  
+- Ran `npm init` and filled config 
+-This sets up your package.json file — the heart of your Node.js/Electron project — which tracks dependencies, scripts, and metadata. 
 - Installed Electron with `npm install --save-dev electron`  
 - Added `"start": "electron ."` in `package.json`  
 - Created `main.js` with a test `console.log()`  
--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow,ipcMain } = require('electron/main')
+const path = require('node:path')
+console.warn('main process')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    // alwaysOnTop: true, //always shows on top of other windows
+    // frame:false,   removes the window frame
+    backgroundColor: '#2e2c29',
+    title: 'My Electron App', //custom title but is overwritten by the index.html title
+  // resizable: false,//   makes the window not resizable
+    webPreferences: {
+      nodeIntegration: true,
+    }
   })
 
   win.loadFile('index.html')
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
